d7b4cd95292835db67f77edee700b1a15a45e753
Steven Zheng <szzheng@ucsd.edu>
2016-02-17 08:27:20 +0000

    added coin flip

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,17 @@ var projects = require('../projects.json');
 
 exports.view = function(req, res){
   	res.render('index', projects);
-  	projects["grid"] = false;
+  	
+  	var random_num = Math.random();
+  	console.log(random_num);
+
+  	if (random_num > 0.5) {
+  		projects["grid"] = false
+  		console.log("Don't redirect");
+  	} else {
+  		res.redirect('/grid');
+  		projects["grid"] = false;
+  	}
 };
 
 exports.viewGrid = function(req, res) {
